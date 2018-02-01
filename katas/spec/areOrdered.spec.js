138ee3978ce2e8f3efd6d6aa801d9d0d000c0ed7
Jonny Rathbone <jonnyrathbone@gmail.com>
2018-02-01 21:14:24 +0000

    added basic project setup

---
+++ katas/spec/areOrdered.spec.js
@@ -0,0 +1,7 @@
+const areOrdered = require('../areOrdered');
+
+describe('areOrdered', () => {
+    it ('describe what happens if this test succeeds here', () => {
+        // expect(areOrdered(???)).to...
+    });
+});
---
+++ katas/spec/maxAndMin.spec.js
@@ -0,0 +1,7 @@
+const maxAndMin = require('../maxAndMin');
+
+describe('maxAndMin', () => {
+    it ('describe what happens if this test succeeds here', () => {
+        // expect(maxAndMin(???)).to...
+    });
+});
